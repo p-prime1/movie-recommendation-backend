c9132d0caf51b93598ee0e12efda8cf17acff9f2
p-prime1 <preciousnwaka95@gmail.com>
2025-07-26 21:54:54 +0100

    fieature: created viewsets for the models

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from .model import Movie, Genre, Rating, UserProfile, Review
+from .serializers import MovieSerializer, GenreSerializer, RatingSerializer, UserProfileSerializer, ReviewSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Moview.objects.all()
+    serializer_class = MovieSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
